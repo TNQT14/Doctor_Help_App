3a7a1a9f6067ae40222bf6ae26265cf81c6a001f
TNQT14 <2002quangthai@gmail.com>
2023-07-27 18:41:26 +0700

    ư4ret
    ưert

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         //Test
+        //Test merge
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
