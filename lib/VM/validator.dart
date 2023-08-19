ec05666ffcc559103d7d2ed1be92b39ada27600f
DuongMinh <tongphungminh@gmail.com>
2023-08-19 15:34:34 +0700

    validator

--- lib/VM/validator.dart
+++ lib/VM/validator.dart
@@ -1,17 +1,44 @@
 
 import 'package:flutter/material.dart';
 
+const EMOJI_REGEXP = r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])';
+const MAX_LENGTH_CHARACTER = 50;
+// const String EMAIL_REGEXP = r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$";
+const String email_regexp = r"^[a-zA-Z0-9+_.-]*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$";
+const String name_regexp = r"^[a-z0-9!#$%&'*+/=?^_`{|}~-]$";
+
 class ValidatedMess{
-  String? validatorPassword(String? text) {
-    if (text!.isEmpty) {
-      return 'Mật khẩu không được trống';
+  String? validatorPassword(String? value) {
+    if(value!.isEmpty){
+      return 'Bạn chưa nhập password.';
     }
-    else if(text!.length<6){
-      return 'Mật khẩu không được ít hơn 6 ký tự';
+    else if(value!.length<6){
+      return 'Password không được ít hơn 6 ký tự.';
     }
-    return null;
+    // else if(RegExp(EMOJI_REGEXP).hasMatch(value)){
+    //   return 'Password không được chứa ký tự đặt biệt.';
+    // }
+    else{
+      return null;
+    }
   }
+
+  String? validatorEmail(String? value) {
+    if (value!.isEmpty) {
+      return 'Bạn chưa nhập Email.';
+    }
+    else if (value!.length < 6) {
+      return 'Email không được ít hơn 6 ký tự.';
+    }
+    else if(!RegExp(email_regexp).hasMatch(value!)){
+      return 'Kiểm tra lại email.';
+    }
+    // else if(value.contains(' ')){
+    //   return 'Email không được có khoản trắng';
+    // }
+    else {
+      return null;
+    }
+  }
+
 }
-
-
-final formKey = GlobalKey<FormState>();
