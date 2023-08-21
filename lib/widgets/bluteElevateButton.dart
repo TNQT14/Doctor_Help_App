389678da473c673e948f34be088edf4c9bfcb381
DuongMinh <tongphungminh@gmail.com>
2023-08-21 16:07:35 +0700

    đổi màu text button

--- lib/widgets/bluteElevateButton.dart
+++ lib/widgets/bluteElevateButton.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../containts/containts.dart';
 
-ElevatedButton blueElevateButton(Function()? tap, String title, {Color color = colorKmain}) {
-  return ElevatedButton(onPressed: tap, child: Text(title, style: txt16w5!.copyWith(color: Colors.white),),
+ElevatedButton blueElevateButton(Function()? tap, String title, {Color color = colorKmain, bool isBlueText = false}) {
+  return ElevatedButton(onPressed: tap, child: Text(title, style: txt16w5!.copyWith(color: isBlueText? colorKmain : Colors.white),),
     style: ElevatedButton.styleFrom(
         shadowColor: Colors.grey,
         elevation: 0,
